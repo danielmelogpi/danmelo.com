b1d07c85befa2c0c31a85d2b47ddce9433151c55
Daniel Melo <danielmelogpi@gmail.com>
2017-02-12 00:21:15 -0200

    gulpfile - css build

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,18 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var minifyCSS = require('gulp-csso');
 
-gulp.task('css', function(){
+gulp.task('css-compile', function(){
   return gulp.src('src/style/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('public/css'))
+});
+
+gulp.task('css-package', function(){
+  return gulp.src('public/css/*.css')
     .pipe(less())
     .pipe(minifyCSS())
     .pipe(gulp.dest('public/css'))
 });
 
-gulp.task('default', [ 'css' ]);
+gulp.task('default', [ 'css-compile' ]);
+gulp.task('production', [ 'css-package' ])
